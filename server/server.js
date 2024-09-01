c59a12e8990f9faf2e3eb21821694216af2285dc
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-09-01 10:19:18 -0700

    complete auth.js on server side

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const db = require('./config/connection');
 // const routes = require('./routes');
 const typeDefs = require("./schemas/typeDefs")
 const resolvers = require("./schemas/resolvers")
+const { authMiddleware } = require("./utils/auth")
 
 const server = new ApolloServer({
   typeDefs,
@@ -22,7 +23,9 @@ const startServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use("/graphql", expressMiddleware(server))
+  app.use("/graphql", expressMiddleware(server, {
+    context: authMiddleware
+  }))
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
